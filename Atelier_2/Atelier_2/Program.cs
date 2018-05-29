2907b4525fd956ef82ad5699431671dc3900046c
derbali-it <derbali.it@gmail.com>
2018-05-29 12:44:08 -0400

    another commit

--- Atelier_2/Atelier_2/Program.cs
+++ Atelier_2/Atelier_2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Atelier_2
 {
@@ -8,7 +9,7 @@ namespace Atelier_2
         static void Main(string[] args)
         {
             //Exercice 1
-            List<String> names = new List<String>(){ "Felix", "Sam", "Xavier", "Pascal", "Éric", "Ben" };
+            /*List<String> names = new List<String>(){ "Felix", "Sam", "Xavier", "Pascal", "Éric", "Ben" };
             foreach (string s in names)
             {
                 Console.Write("\t" + s);
@@ -26,8 +27,22 @@ namespace Atelier_2
             foreach(string s in names)
             {
                 Console.Write("\t" + s);
+            }*/
+
+            //Exercice 2
+            List<String> names = new List<String>() { "Pascal", "Felix", "Sam", "Xavier", "Pascal", "Éric", "Ben", "Felix" };
+            List<string> unique_names = new List<string>();
+        
+            foreach(string name in names)
+            {
+                if (!unique_names.Contains(name))
+                {
+                    unique_names.Add(name);
+                }
             }
-                Console.ReadKey();
+
+            Console.WriteLine("Il y a " + unique_names.Count + " noms distincts");
+            Console.ReadKey();
         }
     }
 }
