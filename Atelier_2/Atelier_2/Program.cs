d17f5a74d08128bd2b3aa5c7b91c125ae702244a
derbali-it <derbali.it@gmail.com>
2018-05-29 12:31:40 -0400

    another commit

--- Atelier_2/Atelier_2/Program.cs
+++ Atelier_2/Atelier_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Atelier_2
 {
@@ -6,7 +7,27 @@ namespace Atelier_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Exercice 1
+            List<String> names = new List<String>(){ "Felix", "Sam", "Xavier", "Pascal", "Éric", "Ben" };
+            foreach (string s in names)
+            {
+                Console.Write("\t" + s);
+            }
+            Console.WriteLine();
+            int i = 2, j = 4;
+
+            for(int index=i; index<=j/2; index++)
+            {
+                    string temp = names[index];
+                    names[index] = names[j - index + i];
+                    names[j - index + i] = temp;
+            }
+           
+            foreach(string s in names)
+            {
+                Console.Write("\t" + s);
+            }
+                Console.ReadKey();
         }
     }
 }
