4f5eefd142f3f26fc349287958ffbdd83736e2cc
derbali-it <derbali.it@gmail.com>
2018-05-29 13:54:00 -0400

    another commit

--- Atelier_2/Atelier_2/Program.cs
+++ Atelier_2/Atelier_2/Program.cs
@@ -84,7 +84,33 @@ namespace Atelier_2
                 Console.WriteLine(s);
             }*/
             //Exercice 6
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee("Felix", new DateTime(2010,05,05), "Marketing", 50000));
+            employees.Add(new Employee("Sam", new DateTime(2009, 06, 14), "Accounting", 42000));
+            employees.Add(new Employee("Ben", new DateTime(2015, 07, 25), "Marketing", 35000));
+            employees.Add(new Employee("Annie", new DateTime(1998, 12, 23), "IT", 70546));
+            employees.Add(new Employee("Myriam", new DateTime(2012, 03, 01), "Accounting", 23564));
+            employees.Add(new Employee("Pascal", new DateTime(2010, 10, 09), "Marketing", 28000));
+            employees.Add(new Employee("Alex", new DateTime(2011, 11, 15), "IT", 64825));
 
+            var req1 = from emp in employees
+                       orderby emp.Hiredate
+                       select emp;
+
+            foreach(var emp in req1)
+            {
+                Console.WriteLine(emp.Name + "\t" + emp.Hiredate) ;
+            }
+
+            var req2 = from emp in employees
+                       where emp.Department.Equals("Marketing")
+                       orderby emp.Name
+                       select emp;
+
+            foreach (var emp in req2)
+            {
+                Console.WriteLine(emp.Name + "\t" + emp.Department);
+            }
 
 
             Console.ReadKey();
