25acee3f71418ea8e0c754539e175d39701e46a5
derbali-it <derbali.it@gmail.com>
2018-05-30 14:32:03 -0400

    another commit

--- Atelier_3/Atelier_3/Controllers/EmployeeController.cs
+++ Atelier_3/Atelier_3/Controllers/EmployeeController.cs
@@ -14,7 +14,7 @@ namespace Atelier_3.Controllers
         public IActionResult Index()
         {
             string output = "<table border=1>";
-            foreach(Employee e in data.employees)
+            foreach (Employee e in data.employees)
             {
                 output += "<tr>";
                 output += "<td>" + e.Id + "</td>";
@@ -23,7 +23,56 @@ namespace Atelier_3.Controllers
                 output += "</tr>";
             }
             output += "</table>";
+            return Content(output, "text/html");
+        }
 
+        public IActionResult SalaryMoreThan(double value)
+        {
+            /*Version sans LINQ
+             * List<Employee> results = new List<Employee>();
+            foreach(Employee e in data.employees)
+            {
+                if(e.Salary >= value)
+                {
+                    results.Add(e);
+                }
+            }*/
+
+            var req = from e in data.employees
+                      where e.Salary >= value
+                      select e;
+
+            string output = "<table border=1>";
+            foreach (Employee e in req)
+            {
+                output += "<tr>";
+                output += "<td>" + e.Id + "</td>";
+                output += "<td>" + e.FirstName + "</td>";
+                output += "<td>" + e.LastName + "</td>";
+                output += "<td>" + e.Salary + "</td>";
+                output += "</tr>";
+            }
+            output += "</table>";
+            return Content(output, "text/html");
+        }
+
+        public IActionResult Department(string name)
+        {
+            var req = from e in data.employees
+                      where e.Department.ToUpper().Equals(name.ToUpper())
+                      select e;
+
+            string output = "<table border=1>";
+            foreach (Employee e in req)
+            {
+                output += "<tr>";
+                output += "<td>" + e.Id + "</td>";
+                output += "<td>" + e.FirstName + "</td>";
+                output += "<td>" + e.LastName + "</td>";
+                output += "<td>" + e.Department+ "</td>";
+                output += "</tr>";
+            }
+            output += "</table>";
             return Content(output, "text/html");
         }
     }
